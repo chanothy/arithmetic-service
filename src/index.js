b2b01464fafee811a1b2a1ed4d5365a4414a0705
Tim Chan <timchan@iu.edu>
2024-04-16 19:21:59 -0400

    updates to arithservice

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
+require("dotenv").config()
 const express = require('express');
 const cors = require('cors');
 const app = express();
 const { add } = require('./arithmetica')
 app.use(cors());
 
-const port = 3000;
+if (!process.env.PORT) {
+    throw new Error (
+        "please specify the port"
+    );
+}
+
+const port = process.env.PORT;
+
 
 app.get('/', (req, res) => {
-    res.send('Arithmetic service - Hello World!');
+    res.send('Arithmetic service - Hello World! Updated 3/5/2024');
 });
 
 app.get('/add/:n/:m', (req, res) => { // any http request with url /add/[number]/[number]
